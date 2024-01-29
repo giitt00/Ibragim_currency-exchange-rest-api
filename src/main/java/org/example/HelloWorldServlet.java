2ca608de2ce0e9e2bd36de06a1355eb3daa55ebb
krios2146 <krios2146@gmail.com>
2024-01-29 17:20:21 +0700

    feat: add HelloWorldServlet

---
+++ src/main/java/org/example/HelloWorldServlet.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/")
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().println("Hello World!");
+    }
+}
