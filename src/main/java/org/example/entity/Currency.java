f4f5a4dd1ed2692b94bc1f09c88df4612e6a77b7
krios2146 <krios2146@gmail.com>
2024-01-29 22:29:23 +0700

    feat: add Currency entity

---
+++ src/main/java/org/example/entity/Currency.java
@@ -0,0 +1,25 @@
+package org.example.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Currency {
+
+    private Long id;
+
+    private String code;
+
+    private String fullName;
+
+    private String sign;
+
+    public Currency(String code, String fullName, String sign) {
+        this.code = code;
+        this.fullName = fullName;
+        this.sign = sign;
+    }
+}
