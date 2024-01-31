2bc61fddd14d320ae3722bd72c955dbe6afd51f8
krios2146 <krios2146@gmail.com>
2024-01-31 17:57:20 +0700

    feat: add EncodingFilter

---
+++ src/main/java/org/example/EncodingFilter.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter("/*")
+public class EncodingFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        res.setCharacterEncoding("UTF-8");
+        req.setCharacterEncoding("UTF-8");
+
+        res.setContentType("application/json");
+
+        super.doFilter(req, res, chain);
+    }
+}
