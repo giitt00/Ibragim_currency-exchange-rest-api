d3653966e6d2f45929a23f8d7dfd26d03b70ddcf
krios2146 <krios2146@gmail.com>
2024-01-31 18:59:06 +0700

    feat: add CurrencyResponseDto

---
+++ src/main/java/org/example/dto/CurrencyResponseDto.java
@@ -0,0 +1,19 @@
+package org.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CurrencyResponseDto {
+
+    private Long id;
+
+    private String name;
+
+    private String code;
+
+    private String sign;
+}
