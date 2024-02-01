5e605be30b58ad1792805fadc6c91630974ebd1a
krios2146 <krios2146@gmail.com>
2024-02-01 16:14:51 +0700

    feat: add ExceptionHandlingFilter

---
+++ src/main/java/org/example/ExceptionHandlingFilter.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.dto.ErrorResponseDto;
+import org.example.exception.DatabaseOperationException;
+import org.example.exception.EntityExistsException;
+import org.example.exception.InvalidParameterException;
+import org.example.exception.NotFoundException;
+
+import java.io.IOException;
+
+import static jakarta.servlet.http.HttpServletResponse.*;
+
+@WebFilter("/*")
+public class ExceptionHandlingFilter extends HttpFilter {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws ServletException, IOException {
+        try {
+            super.doFilter(req, res, chain);
+        }
+        catch (DatabaseOperationException e) {
+            writeErrorResponse(res, SC_INTERNAL_SERVER_ERROR, e);
+        }
+        catch (EntityExistsException e) {
+            writeErrorResponse(res, SC_CONFLICT, e);
+        }
+        catch (InvalidParameterException e) {
+            writeErrorResponse(res, SC_BAD_REQUEST, e);
+        }
+        catch (NotFoundException e) {
+            writeErrorResponse(res, SC_NOT_FOUND, e);
+        }
+    }
+
+    private void writeErrorResponse(HttpServletResponse response, int errorCode, RuntimeException e) throws IOException {
+        response.setStatus(errorCode);
+
+        objectMapper.writeValue(response.getWriter(), new ErrorResponseDto(
+                errorCode,
+                e.getMessage()
+        ));
+    }
+}
