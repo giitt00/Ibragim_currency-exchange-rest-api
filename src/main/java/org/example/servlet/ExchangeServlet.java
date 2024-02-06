446a8faccc04bb713b8e8a0c5db475c579fed773
krios2146 <krios2146@gmail.com>
2024-02-06 15:07:40 +0700

    feat: add ExchangeServlet

---
+++ src/main/java/org/example/servlet/ExchangeServlet.java
@@ -0,0 +1,49 @@
+package org.example.servlet;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.dto.ExchangeRequestDto;
+import org.example.dto.ExchangeResponseDto;
+import org.example.exception.InvalidParameterException;
+import org.example.service.ExchangeService;
+import org.example.utils.ValidationUtils;
+
+import java.io.IOException;
+
+@WebServlet("/exchange")
+public class ExchangeServlet extends HttpServlet {
+
+    private final ExchangeService exchangeService = new ExchangeService();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String baseCurrencyCode = req.getParameter("from");
+        String targetCurrencyCode = req.getParameter("to");
+        String amount = req.getParameter("amount");
+
+        if (amount == null || amount.isBlank()) {
+            throw new InvalidParameterException("Missing parameter - amount");
+        }
+
+        ExchangeRequestDto exchangeRequestDto = new ExchangeRequestDto(baseCurrencyCode, targetCurrencyCode, convertToNumber(amount));
+
+        ValidationUtils.validate(exchangeRequestDto);
+
+        ExchangeResponseDto exchangeResponseDto = exchangeService.exchange(exchangeRequestDto);
+
+        objectMapper.writeValue(resp.getWriter(), exchangeResponseDto);
+    }
+
+    private static Double convertToNumber(String amount) {
+        try {
+            return Double.parseDouble(amount);
+        }
+        catch (NumberFormatException e) {
+            throw new InvalidParameterException("Parameter amount must be a number");
+        }
+    }
+}
