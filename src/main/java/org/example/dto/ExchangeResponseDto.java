c4b07c9fbe4a8920679746da679115de6e7fbe3b
krios2146 <krios2146@gmail.com>
2024-02-06 15:06:47 +0700

    feat: add ExchangeResponseDto

---
+++ src/main/java/org/example/dto/ExchangeResponseDto.java
@@ -0,0 +1,23 @@
+package org.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExchangeResponseDto {
+
+    private CurrencyResponseDto baseCurrency;
+
+    private CurrencyResponseDto targetCurrency;
+
+    private BigDecimal rate;
+
+    private Double amount;
+
+    private Double convertedAmount;
+}
